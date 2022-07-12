c87855c6a30c6a1f62a0eac9c0d4902648090bed
Kohei Keino <keino-k@belong.co.jp>
2022-07-12 19:26:14 +0900

    add winner check and board management

--- ebisample/tictactoe/game.go
+++ ebisample/tictactoe/game.go
@@ -15,10 +15,22 @@ const (
 	Cross  Turn = 1
 )
 
+func (t Turn) String() string {
+	switch t {
+	case Circle:
+		return "○"
+	case Cross:
+		return "×"
+	default:
+		return ""
+	}
+}
+
 type Game struct {
 	images *Images
-
-	turn Turn
+	board  [3][3]string
+	turn   Turn
+	count  int
 }
 
 type Images struct {
@@ -56,16 +68,75 @@ func (g *Game) Update() error {
 	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
 		x, y := ebiten.CursorPosition()
 		opts := &ebiten.DrawImageOptions{}
-		opts.GeoM.Translate(float64(x-60), float64(y-60))
+		// cool code.
+		// opts.GeoM.Translate(float64(160*(x/160+1)-160), float64(160*(y/160+1)-160))
+		opts.GeoM.Translate(getBoardPosition(x), getBoardPosition(y))
+		if !g.isEmpty(x, y) {
+			return nil
+		}
+		g.setInternalBoardPosition(x, y)
 		switch g.turn {
 		case Circle:
 			g.images.board.DrawImage(g.images.circle, opts)
 		case Cross:
 			g.images.board.DrawImage(g.images.cross, opts)
 		}
+		g.count++
+		if g.getWinner() != "" {
+			return nil
+		}
+		if g.checkGameEnd() {
+			g.images.board.Clear()
+			return nil
+		}
 		g.turn ^= 1
 	}
+
 	return nil
+}
+
+func (g *Game) isEmpty(x, y int) bool {
+	return g.board[x/160][y/160] == ""
+}
+
+func (g *Game) setInternalBoardPosition(x, y int) {
+	g.board[x/160][y/160] = g.turn.String()
+}
+
+func (g *Game) checkGameEnd() bool {
+	return g.count == 9
+}
+
+func (g *Game) getWinner() string {
+	for i := range g.board {
+		if isEqual(g.board[i][0], g.board[i][1], g.board[i][2]) {
+			return g.turn.String()
+		}
+		if isEqual(g.board[0][i], g.board[1][i], g.board[2][i]) {
+			return g.turn.String()
+		}
+	}
+	if isEqual(g.board[0][0], g.board[1][1], g.board[2][2]) {
+		return g.turn.String()
+	}
+	if isEqual(g.board[0][2], g.board[1][1], g.board[2][0]) {
+		return g.turn.String()
+	}
+	return ""
+}
+
+func isEqual(s1, s2, s3 string) bool {
+	return s1 != "" && s1 == s2 && s2 == s3
+}
+
+func getBoardPosition(pos int) float64 {
+	if 320 < pos {
+		return 320
+	}
+	if 160 < pos {
+		return 160
+	}
+	return 0
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
